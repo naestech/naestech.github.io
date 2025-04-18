9ac29b077d08163719dd333f6678cc5a2f8a7783
nae! <naesalem@outlook.com>
2025-04-17 22:21:37 -0500

    Trigger deployment

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './components/layout/navbar'
 import Footer from './components/layout/footer'
 import './App.css'
 
+// trigger deployment
 function App() {
   return (
     <div className="app">
